cd396d339ed68cfc0cb052c48e2e143bc7cab487
Abhijit <abhijitkghosh29@gmail.com>
2017-11-05 16:51:50 -0500

    initial structure

---
+++ guessWords.js
@@ -0,0 +1,3 @@
+var words=['Elk','Leopard','Panther','RattleSnake','Koala','Bear','Elephant','Zebra','Giraffe','Tiger','Lion','Alligator','cougar','Turtle','Monkey','Rhinoceros','Panda','Cheetah','Seal','Ostrich'];
+
+var computerGuess= words[Math.floor(Math.random() * words.length)].toLowerCase();
